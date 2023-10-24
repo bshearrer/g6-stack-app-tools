ca28bd17a8356d7186923e4d3d977d60ee510507
Brandon Shearrer <bshearr@outlook.com>
2023-10-24 11:51:49 -0500

    page refactor

--- src/createNextPage.ts
+++ src/createNextPage.ts
@@ -7,63 +7,37 @@ import path = require('path');
 
 export function createNextPage() {
 	return vscode.commands.registerCommand('extension.createNextPage', async () => {
-		const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+		const workspacePath = getWorkspacePath();
 		if (!workspacePath) {
 			vscode.window.showErrorMessage('No workspace detected.');
 			return;
 		}
 
 		const pagesPath = path.join(workspacePath, 'src/pages');
-
 		let currentDir = pagesPath;
-		let dirSelection = null;
+		let dirSelection: string | null = null;
 
 		do {
-			const dirs = readdirSync(currentDir, { withFileTypes: true })
-				.filter((dirent) => dirent.isDirectory())
-				.map((dirent) => '/' + dirent.name);
-
-			const options =
-				currentDir === pagesPath
-					? ['Create File Here', 'Create New Directory', ...dirs]
-					: ['Create File Here', 'Create New Directory', ...dirs, '<- Back'];
-
-			dirSelection = await vscode.window.showQuickPick(options, { placeHolder: 'Select an option or directory' });
-
-			if (!dirSelection) {
-				return; // User canceled
+			dirSelection = await handleDirectorySelection(currentDir, pagesPath);
+			if (dirSelection && dirSelection !== 'Create File Here') {
+				currentDir = dirSelection;
 			}
+		} while (dirSelection && dirSelection !== 'Create File Here');
 
-			if (dirSelection === '<- Back') {
-				currentDir = path.dirname(currentDir);
-			} else if (dirSelection === 'Create New Directory') {
-				const newDirName = await vscode.window.showInputBox({ prompt: 'Enter directory name:' });
-				if (!newDirName) {
-					return; // User canceled
-				}
-				const newDirPath = path.join(currentDir, newDirName);
-				if (!existsSync(newDirPath)) {
-					mkdirSync(newDirPath);
-				}
-				currentDir = newDirPath;
-			} else if (dirSelection !== 'Create File Here') {
-				currentDir = path.join(currentDir, dirSelection);
-			}
-		} while (dirSelection !== 'Create File Here');
+		if (!dirSelection) {
+			return;
+		}
 
-		let pageName = await vscode.window.showInputBox({ prompt: 'Enter page name:' });
+		const pageName = await getPageName();
 		if (!pageName) {
 			vscode.window.showErrorMessage('Page name is required!');
 			return;
 		}
 
-		let featureName: string | undefined = pageName;
-		if (pageName.toLowerCase() === 'index') {
-			featureName = await vscode.window.showInputBox({ prompt: 'Enter feature name for index page:' });
-			if (!featureName) {
-				vscode.window.showErrorMessage('Feature name is required for index page!');
-				return;
-			}
+		const featureName = await getFeatureName(pageName);
+		if (!featureName) {
+			vscode.window.showErrorMessage('Feature name is required for index page!');
+			return;
 		}
 
 		const formattedPageName = lowerCaseFirstLetter(pageName.replace(/\.tsx|\.ts|\.jsx|\.js/g, ''));
@@ -73,9 +47,7 @@ export function createNextPage() {
 		const fullPagePath = path.join(pagesPath, `${normalizedPath}.tsx`);
 		const pageDirectory = path.dirname(fullPagePath);
 
-		if (!existsSync(pageDirectory)) {
-			mkdirSync(pageDirectory, { recursive: true });
-		}
+		createDirectories([pageDirectory]);
 
 		if (existsSync(fullPagePath)) {
 			vscode.window.showErrorMessage(`Page "${normalizedPath}.tsx" already exists.`);
@@ -89,24 +61,86 @@ export function createNextPage() {
 		const featureFilePath = path.join(workspacePath, `/src/features/screens/${formattedFeatureName}/index.tsx`);
 		const featureDirectory = path.dirname(featureFilePath);
 
-		if (!existsSync(featureDirectory)) {
-			mkdirSync(featureDirectory, { recursive: true });
-		}
+		createDirectories([featureDirectory, featureFilePath]);
 
 		writeFileSync(featureFilePath, getFeatureTemplate(formattedFeatureName), 'utf-8');
 		formatFileWithPrettier(featureFilePath);
 
-		const componentPath = path.join(featureDirectory, 'components');
-		const commonPath = path.join(featureDirectory, 'common');
-		const utilPath = path.join(commonPath, 'util');
-		const hooksPath = path.join(commonPath, 'hooks');
+		const dirsToCreate = [
+			path.join(featureDirectory, 'components'),
+			path.join(featureDirectory, 'common'),
+			path.join(featureDirectory, 'common', 'util'),
+			path.join(featureDirectory, 'common', 'hooks'),
+		];
 
-		[componentPath, commonPath, utilPath, hooksPath].forEach((dir) => {
-			if (!existsSync(dir)) {
-				mkdirSync(dir, { recursive: true });
-			}
-		});
-
+		createDirectories(dirsToCreate);
 		vscode.window.showInformationMessage(`Feature "${formattedFeatureName}/index.tsx" created successfully!`);
+	});
+}
+
+function getWorkspacePath(): string | null {
+	return vscode.workspace.workspaceFolders?.[0].uri.fsPath || null;
+}
+
+function getDirectories(currentDir: string): string[] {
+	return readdirSync(currentDir, { withFileTypes: true })
+		.filter((dirent) => dirent.isDirectory() && dirent.name !== 'api')
+		.map((dirent) => '/' + dirent.name);
+}
+
+async function handleDirectorySelection(currentDir: string, pagesPath: string): Promise<string | null> {
+	const dirs = getDirectories(currentDir);
+	const options =
+		currentDir === pagesPath
+			? ['Create File Here', 'Create New Directory', ...dirs]
+			: ['Create File Here', 'Create New Directory', ...dirs, '<- Back'];
+
+	const dirSelection = await vscode.window.showQuickPick(options, { placeHolder: 'Select an option or directory' });
+
+	if (!dirSelection) {
+		return null;
+	} // User canceled
+
+	if (dirSelection === '<- Back') {
+		return path.dirname(currentDir);
+	}
+
+	if (dirSelection === 'Create New Directory') {
+		const newDirName = await vscode.window.showInputBox({ prompt: 'Enter directory name:' });
+		if (!newDirName) {
+			return null;
+		} // User canceled
+
+		const newDirPath = path.join(currentDir, newDirName);
+		if (!existsSync(newDirPath)) {
+			mkdirSync(newDirPath);
+		}
+		return newDirPath;
+	}
+
+	if (dirSelection !== 'Create File Here') {
+		return path.join(currentDir, dirSelection);
+	}
+
+	return dirSelection;
+}
+
+async function getPageName(): Promise<string | null | undefined> {
+	return await vscode.window.showInputBox({ prompt: 'Enter page name:' });
+}
+
+async function getFeatureName(pageName: string): Promise<string | null | undefined> {
+	if (pageName.toLowerCase() !== 'index') {
+		return pageName;
+	}
+
+	return await vscode.window.showInputBox({ prompt: 'Enter feature name for index page:' });
+}
+
+function createDirectories(paths: string[]): void {
+	paths.forEach((dir) => {
+		if (!existsSync(dir)) {
+			mkdirSync(dir, { recursive: true });
+		}
 	});
 }
